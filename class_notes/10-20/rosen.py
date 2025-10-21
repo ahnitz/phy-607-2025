4d4fe04a3401c9c679ec4bcc06389e7ac9279a73
Alexander Harvey Nitz <alex.nitz@gmail.com>
2025-10-21 11:58:53 -0400

    update reference plots

--- class_notes/10-20/rosen.py
+++ class_notes/10-20/rosen.py
@@ -1,25 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy
+
+from matplotlib import pyplot as plt
 from scipy.optimize import rosen
 
-# Create a grid of points in the x-y plane
-x = np.linspace(-2, 2, 400)
-y = np.linspace(-1, 3, 400)
-X, Y = np.meshgrid(x, y)
+x = numpy.arange(-2, 2, .01)
+y = numpy.arange(-1, 3, .01)
+X, Y = numpy.meshgrid(x, x)
 
-# Initialize an array to store Rosenbrock function values
-Z = np.zeros_like(X)
-
-# Calculate the Rosenbrock function for each point on the grid
-for i in range(X.shape[0]):
-    for j in range(X.shape[1]):
-        Z[i, j] = rosen([X[i, j], Y[i, j]])
-
-# Create the plot
-plt.figure(figsize=(8, 6))
-plt.pcolormesh(X, Y, Z, cmap='plasma')
-plt.title('Rosenbrock Function (using scipy.optimize.rosen)')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.colorbar(label='f(x, y)')
+z = rosen((X, Y))
+plt.pcolormesh(X, Y, z, norm='log', vmin=1e-3)
+c = plt.colorbar()
 plt.show()
